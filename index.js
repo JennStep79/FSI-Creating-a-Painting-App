0d92a0030ce7bc3d2c63c6e60b3576c654c157ff
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2022-08-05 18:13:08 -0700

    changed pixel selector to paint blue

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+let selectedColor = 'blue'
 const painting = document.querySelector('.painting')
 painting.addEventListener('click', function(e){
-    console.log(e.target)
+    e.target.style.backgroundColor = selectedColor
 })
